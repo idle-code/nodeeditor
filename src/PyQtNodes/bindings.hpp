fd3d65fd5d2464ae908146576eb3e607207aacfd
Pawel Zukowski <p.z.idlecode@gmail.com>
2018-08-15 20:12:56 +0200

    WIP: Starting with shiboken2 generation

---
+++ src/PyQtNodes/bindings.hpp
@@ -0,0 +1,16 @@
+#define NODE_EDITOR_SHARED
+//#include <nodes/Connection>
+//#include <nodes/ConnectionStyle>
+#include <nodes/DataModelRegistry>
+#include <nodes/FlowScene>
+#include <nodes/FlowView>
+//#include <nodes/FlowViewStyle>
+#include <nodes/Node>
+#include <nodes/NodeData>
+#include <nodes/NodeDataModel>
+//#include <nodes/NodeGeometry>
+//#include <nodes/NodePainterDelegate>
+//#include <nodes/NodeState>
+//#include <nodes/NodeStyle>
+//#include <nodes/TypeConverter>
+
---
+++ src/PyQtNodes/run_shiboken.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/zsh
+SHIBOKEN="/home/idlecode/.virtualenvs/PyNodeEditor-PhDSOCn3/lib/python3.6/site-packages/PySide2/shiboken2"
+$SHIBOKEN \
+  --enable-parent-ctor-heuristic \
+  --enable-pyside-extensions \
+  --system-include-paths=/usr/local/Qt-5.11.0/include \
+  --include-paths=../../include \
+  --output-directory=bindings.generated/ \
+  bindings.hpp \
+  bindings.xml
+
