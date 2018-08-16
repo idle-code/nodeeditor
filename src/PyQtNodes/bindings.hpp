2854a1a8a2f2ad319c81b51699a91607fff35f6e
Pawel Zukowski <p.z.idlecode@gmail.com>
2018-08-16 20:16:07 +0200

    Include all headers and types in binding generation process

--- src/PyQtNodes/bindings.hpp
+++ src/PyQtNodes/bindings.hpp
@@ -1,16 +1,17 @@
 #define NODE_EDITOR_SHARED
-//#include <nodes/Connection>
-//#include <nodes/ConnectionStyle>
+
+#include <nodes/Connection>
+#include <nodes/ConnectionStyle>
 #include <nodes/DataModelRegistry>
 #include <nodes/FlowScene>
 #include <nodes/FlowView>
-//#include <nodes/FlowViewStyle>
+#include <nodes/FlowViewStyle>
 #include <nodes/Node>
 #include <nodes/NodeData>
 #include <nodes/NodeDataModel>
-//#include <nodes/NodeGeometry>
-//#include <nodes/NodePainterDelegate>
-//#include <nodes/NodeState>
-//#include <nodes/NodeStyle>
-//#include <nodes/TypeConverter>
+#include <nodes/NodeGeometry>
+#include <nodes/NodePainterDelegate>
+#include <nodes/NodeState>
+#include <nodes/NodeStyle>
+#include <nodes/TypeConverter>
 
--- src/PyQtNodes/run_shiboken.sh
+++ src/PyQtNodes/run_shiboken.sh
@@ -6,6 +6,7 @@ $SHIBOKEN \
   --system-include-paths=/usr/local/Qt-5.11.0/include \
   --include-paths=../../include \
   --output-directory=bindings.generated/ \
+  --debug-level=full \
   bindings.hpp \
   bindings.xml
 
